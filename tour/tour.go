f640eebd7ff8da1d9eff3966ac9651d4bb85f18d
Geoff Moller <geoff.moller@gmail.com>
2013-08-04 19:23:13 -0700

    ex23

--- tour/tour.go
+++ tour/tour.go
@@ -5,20 +5,18 @@ import (
   "math"
 )
 
-func pow(x, n, lim float64) float64 {
-  if v:= math.Pow(x, n); v < lim {
-    return v
-  } else {
-    fmt.Printf("%g >= %g\n", v, lim)
+func Sqrt(x float64, z float64) float64 {
+  for {
+    var temp = z - ((math.Pow(z, 2) - x) / (2 * z))
+    if temp == z {
+      return z
+    } else {
+      z = temp
+    }
   }
-  //can't use v here, though
-  return lim
 }
 
 func main(){
-  fmt.Println(
-    pow(3, 2, 10),
-    pow(3, 3, 10),
-  )
+  fmt.Println(Sqrt(2000, 1))
 }
 
