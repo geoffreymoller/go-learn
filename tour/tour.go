8c5e51dd2ec0efed5a48cea17f4177226fa575af
Geoff Moller <geoff.moller@gmail.com>
2013-08-05 22:15:05 -0700

    ex33 range for

--- tour/tour.go
+++ tour/tour.go
@@ -2,19 +2,12 @@ package main
 
 import "fmt"
 
+var pow = []int{1,2,4,8,16,32,64}
+
 func main(){
-  a := make([]int, 5)
-  printSlice("a", a)
-  b := make([]int, 0, 5)
-  printSlice("b", b)
-  c := b[:2]
-  printSlice("c", c)
-  d := c[2:5]
-  printSlice("d", d)
+  for i, v:= range pow {
+    fmt.Printf("2**%d = %d\n", i, v)
+  }
 }
 
-func printSlice(s string, x []int){
-  fmt.Printf("%s len=%d cap=%d %v\n",
-    s, len(x), cap(x), x)
-}
 
