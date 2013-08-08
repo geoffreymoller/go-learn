caae0fa6805c3236a4d45cb03ec62e85477e7a76
Geoff Moller <geoff.moller@gmail.com>
2013-08-07 19:15:32 -0700

    ex36 maps

--- tour/tour.go
+++ tour/tour.go
@@ -2,23 +2,20 @@ package main
 
 
 import (
-  "code.google.com/p/go-tour/pic"
+  "fmt"
 )
 
-func Pic(dx, dy int) [][]uint8 {
-  ary := make([][]uint8, dy)
-  for i := 0; i < dy; i++ {
-    ary2 := make([]uint8, dx)
-    for j := 0; j < dx; j++ {
-      var pixel = uint8(j * i)
-      ary2[j] = pixel
-    }
-    ary[i] = ary2
-  }
-  return ary
+type Vertex struct {
+  Lat, Long float64
 }
 
+var m map[string]Vertex
+
 func main(){
-  pic.Show(Pic)
+  m = make(map[string]Vertex)
+  m["Bell Labs"] = Vertex {
+    40.68433, -74.39967,
+  }
+  fmt.Println(m["Bell Labs"])
 }
 
