4ecf06b74d5f1b29ae1e6eab40267451d03db523
Geoff Moller <geoff.moller@gmail.com>
2013-08-08 22:36:21 -0700

    ex43 fibonacci closure

--- tour/tour.go
+++ tour/tour.go
@@ -4,21 +4,28 @@ import (
   "fmt"
 )
 
-func adder() func(int) int {
-  sum := 0
-  return func(x int) int {
-    sum += x
+func fibonacci() func() int {
+  var count, sum, first, second int
+  return func() int {
+    if count == 0 {
+      count++
+      return 0
+    } else if count == 1 {
+      second = count
+      count++
+      return 1
+    }
+    sum = first + second
+    first = second
+    second = sum
     return sum
   }
 }
 
 func main(){
-  pos, neg := adder(), adder()
+  f := fibonacci()
   for i := 0; i < 10; i++ {
-    fmt.Println(
-      pos(i),
-      neg(-2*i),
-    )
+    fmt.Println(f());
   }
 }
 
