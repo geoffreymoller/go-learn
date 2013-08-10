c3ddabe0f1f6c704c5a539e4ac788efe14270ca7
Geoff Moller <geoff.moller@gmail.com>
2013-08-10 15:35:54 -0700

    ex47 cube roots with real numbers

--- tour/tour.go
+++ tour/tour.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-  "fmt"
-  "runtime"
+	"fmt"
+	"math"
 )
 
-func main(){
-  fmt.Print("Go runs on ")
-  switch os := runtime.GOOS; os {
-    case "darwin":
-      fmt.Println("OS X.")
-    case "linuk":
-      fmt.Println("Linux.")
-    default:
-      //freebsd, openbsd,
-      //plan9, windows...
-      fmt.Printf("%s.", os)
-  }
+func Cbrt(x float64, z float64) float64 {
+	for {
+		var temp = z - ((math.Pow(z, 3) - x) / (3 * math.Pow(z, 2)))
+		if temp == z {
+			return z
+		} else {
+			z = temp
+		}
+	}
+}
+
+func main() {
+	fmt.Println(Cbrt(2, 1.33))
 }
 
