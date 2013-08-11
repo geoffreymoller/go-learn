488380509a587664939cbbd6d25f3b62593edac1
Geoff Moller <geoff.moller@gmail.com>
2013-08-11 16:22:24 -0700

    ex57 HTTP Handlers

--- tour/tour.go
+++ tour/tour.go
@@ -5,14 +5,25 @@ import (
 	"net/http"
 )
 
-type Hello struct{}
+type String string
 
-func (h Hello) ServeHTTP(w http.ResponseWriter, r *http.Request){
-  fmt.Fprint(w, "Hello!")
+func (s String) ServeHTTP(w http.ResponseWriter,  r *http.Request) {
+  fmt.Fprint(w, "I'm a frayed knot.")
+}
+
+type Struct struct {
+  Greeting string
+  Punct string
+  Who string
+}
+
+func (f *Struct) ServeHTTP(w http.ResponseWriter,  r *http.Request) {
+  fmt.Fprint(w, f.Greeting + " " + f.Punct + " " + f.Who)
 }
 
 func main() {
-  var h Hello
-  http.ListenAndServe("localhost:4000", h)
+  http.Handle("/string", String("I'm a frayed knot."))
+  http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+  http.ListenAndServe("localhost:4000", nil)
 }
 
